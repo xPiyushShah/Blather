fb729f3c94d24bc855494b4db03623a663cb0934
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-09 22:38:29 +0530

    Update axios.js

--- Front-end/src/libs/axios.js
+++ Front-end/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://blather.onrender.com/api",
   withCredentials: true, 
 });
