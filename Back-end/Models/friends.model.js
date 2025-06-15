b804dc05da7e9a40645dbec82a539d211323f226
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-15 15:15:32 +0530

    get

--- Back-end/Models/friends.model.js
+++ Back-end/Models/friends.model.js
@@ -1,1 +1,31 @@
-0
+
+import mongoose from "mongoose";
+
+const FriendSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    friendId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Friend = mongoose.models.Friend || mongoose.model("Friend", FriendSchema);
+export default Friend;
