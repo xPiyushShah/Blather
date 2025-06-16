7297f3d40dafa627cedd2b9c2985e5ae957ab316
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-16 19:01:16 +0530

    Update utils.js

--- Back-end/libs/utils.js
+++ Back-end/libs/utils.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
 export const gToken = (userId, res) => {
-  const token = jwt.sign({ userId }, "MYSHARKTANK", {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
   res.cookie("auth_token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
   });
 };
