847214af7ccec6f29454ff700dbddd7654823d0f
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-18 23:40:57 +0530

    new client url

--- Back-end/server.js
+++ Back-end/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "https://blather-psi.vercel.app",
+    origin: "https://blather-cyan.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
   })
