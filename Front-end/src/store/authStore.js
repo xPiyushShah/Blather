43868670e8497d69ebe89dcbee12af03f35a49d7
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-19 09:26:35 +0530

    auth

--- Front-end/src/store/authStore.js
+++ Front-end/src/store/authStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../libs/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = " https://blather.onrender.com";
+const BASE_URL = "https://blather.onrender.com";
 
 export const authStore = create((set, get) => ({
   authUser: null,
