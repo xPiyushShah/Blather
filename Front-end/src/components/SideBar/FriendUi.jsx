6c4d01fa4db1052b047ba749bcefe2d484536066
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-20 01:32:04 +0530

    t

--- Front-end/src/components/SideBar/FriendUi.jsx
+++ Front-end/src/components/SideBar/FriendUi.jsx
@@ -15,8 +15,8 @@ function FriendUi() {
             <div
               key={item._id}
               className={`h-[4rem] border-b-[1px] border-b-[#dddddd35] w-full flex flex-row justify-between items-center text-center ${
-                selectedUser === item._id
-                  ? "bg-red-300 ring-1 ring-base-300 "
+                selectedUser._id === item._id
+                  ? "bg-base-300 ring-1 ring-base-300 "
                   : ""
               } mb-4`}
               style={{ cursor: "pointer", padding: "2px 16px" }}
