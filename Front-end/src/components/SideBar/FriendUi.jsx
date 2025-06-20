65b810cdb3dd1d52661c61f24ddbe5cdbe9d5057
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-20 14:17:15 +0530

    w

--- Front-end/src/components/SideBar/FriendUi.jsx
+++ Front-end/src/components/SideBar/FriendUi.jsx
@@ -5,8 +5,9 @@ import ListSkelecton from "./ListSkelecton";
 import ProfileAvatar from "../ProfileAvatar";
 
 function FriendUi() {
-  const { users, friendList, selectedUser , setSelctedUser} = useChatStore();
-    const { onlineUser, authUser, socket, addfriend } = authStore();
+  const { users, friendList, selectedUser, setSelctedUser } = useChatStore();
+  const { onlineUser, authUser, socket, addfriend } = authStore();
+  const selctedID = selectedUser ? selectedUser._id === item._id : false;
   return (
     <>
       {friendList && (
@@ -14,20 +15,18 @@ function FriendUi() {
           {friendList?.map((item) => (
             <div
               key={item._id}
-              className={`h-[4rem] border-b-[1px] border-b-[#dddddd35] w-full flex flex-row justify-between items-center text-center ${
-                selectedUser._id === item._id
+              className={`h-[4rem] border-b-[1px] border-b-[#dddddd35] w-full flex flex-row justify-between items-center text-center ${selctedID
                   ? "bg-base-300 ring-1 ring-base-300 "
                   : ""
-              } mb-4`}
+                } mb-4`}
               style={{ cursor: "pointer", padding: "2px 16px" }}
               onClick={() => setSelctedUser(item)}>
               <div>
                 <div
-                  className={`avatar text-center align-center ${
-                    onlineUser.includes(item._id)
+                  className={`avatar text-center align-center ${onlineUser.includes(item._id)
                       ? "avatar-online"
                       : "avatar-offline"
-                  }`}>
+                    }`}>
                   <div className="w-8 mask mask-squircle">
                     {item.profile_url ? (
                       <img
