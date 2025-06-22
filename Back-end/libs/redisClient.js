da74a5bfe1927f3ec2997ba041cc00d968a5df92
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2025-06-22 11:46:01 +0530

    w

--- Back-end/libs/redisClient.js
+++ Back-end/libs/redisClient.js
@@ -1,8 +1,10 @@
-import redis from 'redis';
+import redis from "redis";
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  url: "redis://red-d1bpu63e5dus73erkemg:6379",
+});
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 await redisClient.connect();
 
